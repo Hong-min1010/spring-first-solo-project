c7ed96eda7ec9ffde1dfb7c580fcf033d8bb6b84
Hong-min1010 <qza7367@gmail.com>
2025-02-18 09:08:42 +0900

    fix: Answer Exception Update

--- src/main/java/com/springboot/exception/ExceptionCode.java
+++ src/main/java/com/springboot/exception/ExceptionCode.java
@@ -6,12 +6,13 @@ public enum ExceptionCode {
     USER_NOT_FOUND(404, "User not found"),
     USER_EXISTS(409, "User exists"),
     QUESTION_NOT_FOUND(404, "Question not found"),
-    ANSWER_NOT_FOUND(404, "Answer not found"),
+    ANSWER_ALREADY_EXISTS(409, "Answer already exists"),
     QUESTION_FORBIDDEN(403, "Forbidden Question"),
     LIKE_ALREADY_EXISTS(409, "Like already exists"),
     LIKE_NOT_FOUND(404, "Like not found"),
     USER_NOT_CREATED(400, "User not created"),
-    USER_FORBIDDEN(403, "Forbidden User");
+    USER_FORBIDDEN(403, "Forbidden User"),
+    ANSWER_NOT_FOUND(404, "Answer not found");
 
     @Getter
     private int status;
